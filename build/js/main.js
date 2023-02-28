0be011b57a117c24bc23108d14035995c4e2441a
Elvinas Jonusas <elvinasjon@gmail.com>
2023-03-01 00:30:25 +0200

    added email form to contact us and script to receive it

--- build/js/main.js
+++ build/js/main.js
@@ -13,3 +13,28 @@ const initApp = () => {
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
+
+  const form = document.querySelector('#contact-form');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    fetch('/send-email.php', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => {
+      if (response.ok) {
+        alert('Your message has been sent!');
+        form.reset();
+      } else {
+        throw new Error('An error occurred while sending your message.');
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      alert('An error occurred while sending your message. Please try again later.');
+    });
+  });
